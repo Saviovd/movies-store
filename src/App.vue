9999689b408c40ca514d08045604465c2f1bc0fb
Saviovd <savio.pinheiroal@gmail.com>
2025-02-03 17:30:26 -0400

    📦 NEW: first commit
    - initial settings to prettier and eslint
    - including tailwindcss to style app
    - including axios to fetch movies data after

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <div class="app flex h-screen w-full items-center justify-center" @click="increment">
+    {{ count }}
+  </div>
+</template>
+
+<style scoped></style>
