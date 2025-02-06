7e888d5e38199f19453f1863da6c2170c70f2e4b
Saviovd <savio.pinheiroal@gmail.com>
2025-02-05 23:55:17 -0400

    🐛 FIX: try to fix route error in vercel 3
    - import with alias

--- src/shims-vue.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<object, object, any>
-  export default component
-}
